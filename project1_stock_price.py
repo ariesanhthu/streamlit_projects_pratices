90191b2c1ad2c9c21972509a39c097a4276bf4c5
unknown <anhthuxitin@outlook.com>
2025-03-01 01:16:01 +0700

    req

--- project1_stock_price.py
+++ project1_stock_price.py
@@ -1,31 +1,7 @@
 import yfinance as yf
 import streamlit as st
 
-################################################################
-## VERSION SIMPLE
-
-# st.write("""
-# # Simple Stock Price App
-
-# Shown are the stock closing price and volume of Google!
-
-# """)
-
-# #define the ticker symbol
-# tickerSymbol = 'GOOGL'
-
-# #get data on this ticker
-# tickerData = yf.Ticker(tickerSymbol)
-
-# #get the historical prices for this ticker
-# tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
-
-# # Open	High	Low	Close	Volume	Dividends	Stock Splits
-
-# st.line_chart(tickerDf.Close)
-
-# st.line_chart(tickerDf.Volume)
-
+import datetime
 ################################################################
 ## VERSION 2
 import yfinance as yf
@@ -37,14 +13,21 @@ st.write("""
 Nhập ticker symbol và xem biểu đồ giá đóng cửa và khối lượng giao dịch.
 """)
 
+# INPUT
+
 # Nhận ticker symbol từ người dùng, mặc định là 'GOOGL'
 tickerSymbol = st.text_input('Nhập ticker symbol:', 'GOOGL')
+
+start_date = st.date_input("Chọn ngày bắt đầu:", datetime.date(2010, 5, 31))
+end_date = st.date_input("Chọn ngày kết thúc:", datetime.date(2020, 5, 31))
+
+# ----------------------------------------------------------------
 
 # Lấy dữ liệu của ticker từ yfinance
 tickerData = yf.Ticker(tickerSymbol)
 
 # Lấy dữ liệu lịch sử từ ngày 31/05/2010 đến 31/05/2020
-tickerDf = tickerData.history(period='1d', start='2025-02-20', end='2025-05-27')
+tickerDf = tickerData.history(period='1d', start=start_date, end=end_date)
 
 # Hiển thị biểu đồ giá đóng cửa
 st.write(f"### Giá đóng cửa của {tickerSymbol}")
